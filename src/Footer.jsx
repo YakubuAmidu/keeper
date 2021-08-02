2e74d675d084ee8b47141f166dc984f909ae31d4
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-08-02 00:39:49 -0700

    Keeper app

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <hp>&copy; 2021</hp>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/Note.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Note = () => {
+  return (
+    <div>
+      <h1>Note.jsx</h1>
+      <p>This where content goes</p>
+    </div>
+  );
+};
+
+export default Note;
