fdea1313f15e47690d9b88048221e2e88a5cf1b7
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2021-08-05 04:25:13 -0700

    Initial commit

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function CreateArea() {
+  const [note, setNote] = useState({
+    title: '',
+    content: '',
+  });
+
+  const [itemValues, setItemValues] = useState([]);
+
+  function handleChange(event) {
+    const newItem = event.target.value;
+    setItems(newItem);
+  }
+
+  function handleClick(event) {
+    console.log('Clicked');
+    setItemValues(items);
+
+    event.preventDefault();
+  }
+
   return (
     <div>
       <form>
-        <input name='title' placeholder='Title' />
-        <textarea name='content' placeholder='Take a note...' rows='3' />
-        <button>Add</button>
+        <input
+          name='title'
+          value={note.title}
+          placeholder='Title'
+          value='item'
+          onChange={handleChange}
+        />
+        <textarea
+          name='content'
+          value={note.content}
+          placeholder='Take a note...'
+          rows='3'
+          onChange={handleChange}
+        />
+        <button onClick={handleClick}>Add</button>
       </form>
     </div>
   );
